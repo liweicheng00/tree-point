6d2b0c9dbf70b0e29fee8dbfcc8c7818c3be2543
liweicheng00 <47774646+liweicheng00@users.noreply.github.com>
2023-02-13 18:30:01 +0800

    update database url for docker-compose

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://root:mysqlsecret@127.0.0.1:3306/treepoint"
-# SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://root:mysqlsecret@mysql/treepoint"
+# SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://root:mysqlsecret@127.0.0.1:3306/treepoint"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://root:mysqlsecret@mysql/treepoint"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
